0dd102672c56678f7fcb53afb57959c4e77a3545
Eduardo Henao <eduardo.henao@splicedigital.com>
2020-06-20 00:39:21 -0400

    formated html on php

--- functions.php
+++ functions.php
@@ -19,27 +19,23 @@ add_action( 'wp_footer', 'blankslate_footer_scripts' );
 function blankslate_footer_scripts() {
 ?>
 <script>
-jQuery(document).ready(function ($) {
-var deviceAgent = navigator.userAgent.toLowerCase();
-if (deviceAgent.match(/(iphone|ipod|ipad)/)) {
-$("html").addClass("ios");
-$("html").addClass("mobile");
-}
-if (navigator.userAgent.search("MSIE") >= 0) {
-$("html").addClass("ie");
-}
-else if (navigator.userAgent.search("Chrome") >= 0) {
-$("html").addClass("chrome");
-}
-else if (navigator.userAgent.search("Firefox") >= 0) {
-$("html").addClass("firefox");
-}
-else if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
-$("html").addClass("safari");
-}
-else if (navigator.userAgent.search("Opera") >= 0) {
-$("html").addClass("opera");
-}
+jQuery(document).ready(function($) {
+    var deviceAgent = navigator.userAgent.toLowerCase();
+    if (deviceAgent.match(/(iphone|ipod|ipad)/)) {
+        $("html").addClass("ios");
+        $("html").addClass("mobile");
+    }
+    if (navigator.userAgent.search("MSIE") >= 0) {
+        $("html").addClass("ie");
+    } else if (navigator.userAgent.search("Chrome") >= 0) {
+        $("html").addClass("chrome");
+    } else if (navigator.userAgent.search("Firefox") >= 0) {
+        $("html").addClass("firefox");
+    } else if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
+        $("html").addClass("safari");
+    } else if (navigator.userAgent.search("Opera") >= 0) {
+        $("html").addClass("opera");
+    }
 });
 </script>
 <?php
